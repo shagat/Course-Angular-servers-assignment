feca9854533ef68edff83b268b55ea0c0151749c
shagat <shagat.bing@gmail.com>
2022-05-25 14:18:49 -0400

    structural directives and attribute directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,20 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
-export class ServerComponent {
-    username: string = ' ';
-    isValid: boolean = false
-    
-    validateUsername(event: Event){
-        this.username = (<HTMLInputElement>event.target).value
-        if (this.username != ' '){
-            return this.isValid = true
-        }
-        else{
-            return this.isValid = false
-        }
+export class ServerComponent implements OnInit{
+
+    serverId: number = 10;
+    serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green':'red';
     }
 }
